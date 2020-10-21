3058c5b1091d9cecfcbc1497c101bbbd5e57ce08
Elfydia <elfydia@gmail.com>
2020-10-21 09:48:04 +0200

    add meta google site verif

--- src/view/header.php
+++ src/view/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="google-site-verification: googleb315efc55d9612ea.html" />
     <link rel="stylesheet" href="node_modules/bulma/css/bulma.min.css">
     <link rel="stylesheet" href="style.css">
     <title>Document</title>
